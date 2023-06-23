00d42376e970b3d72513844e921074c688d88611
“Ganeshp0296” <Ganeshp457@gmail.com>
2023-06-23 22:21:25 +0530

    Added:Code for last name

--- src/com/bl/regex/UserRegistration.java
+++ src/com/bl/regex/UserRegistration.java
@@ -3,12 +3,15 @@ package com.bl.regex;
 import java.util.regex.*;
 public class UserRegistration {
     public static void main( String[] args ) {
-        System.out.println(firstName("Tom"));
-
+        System.out.println(firstName("Ganesh"));
+        System.out.println(lastName("Pawar"));
     }
     // validate first name
     public static boolean firstName( String firstName ) {
-        return firstName.matches( "[A-Z][a-z]*" );
+        return firstName.matches( "[A-Z][a-z]{3,}" );
     }
-
+    // validate last name
+    public static boolean lastName( String lastName ) {
+        return lastName.matches( "[A-Z][a-z]{3,}" );
+    }
 }
