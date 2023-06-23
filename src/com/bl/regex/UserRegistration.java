809df982cd83340a78c3c8533ef22dd215279572
“Ganeshp0296” <Ganeshp457@gmail.com>
2023-06-23 22:28:45 +0530

    Added:Code to validate email

--- src/com/bl/regex/UserRegistration.java
+++ src/com/bl/regex/UserRegistration.java
@@ -5,6 +5,7 @@ public class UserRegistration {
     public static void main( String[] args ) {
         System.out.println(firstName("Ganesh"));
         System.out.println(lastName("Pawar"));
+        System.out.println(eMail("Ganesh02@gmail.com"));
     }
     // validate first name
     public static boolean firstName( String firstName ) {
@@ -13,5 +14,9 @@ public class UserRegistration {
     // validate last name
     public static boolean lastName( String lastName ) {
         return lastName.matches( "[A-Z][a-z]{3,}" );
+    }
+    //validate Email
+    public static boolean eMail(String eMail){
+        return eMail.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
     }
 }
